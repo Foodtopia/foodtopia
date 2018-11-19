70373ae669dbc5035e817f81730ba0329fd9f67f
yvonne <yvonne250003@gmail.com>
2018-11-19 18:14:49 +0800

    recipe page add header and ingredients part

--- src/components/recipe_page/recipe_page.js
+++ src/components/recipe_page/recipe_page.js
@@ -12,28 +12,44 @@ class Recipe_page extends Component {
     return (
       <BrowserRouter>
         <React.Fragment>
-          <div className="head_bg">
-            <div className="container d-flex">
+          <main className="head_bg">
+            <header className=" container d-flex">
                 <img className="main_pic" src={require('./images/western_2_main.jpg')}/>
                 <div className="main_text">
                     <h1>大蒜奶油沙朗牛排佐帕瑪森起司馬鈴薯</h1>
                     <h4>大蒜、香草、奶油溶入牛排中，帶出豐富有層次的口感，這種晚餐將會是一種享受。</h4>
                     <div className="cook_info_wrap d-flex">
                         <div className="cook_info ">
-                            <img className="time " src={require('./images/clock.svg')}/>
-                            {/* <p>40min</p> */}
+                            <img className="info_icon time" src={require('./images/clock.svg')}/>
+                            <h3>40min</h3>
                         </div>
-                        <div className="cook_info level"></div>
-                        <div className="cook_info portion"></div>
+                        <div className=" cook_info ">
+                            <img className="info_icon level" src={require('./images/chef.svg')}/>
+                            <h3>容易</h3>
+                        </div>
+                        <div className=" cook_info ">
+                            <img className="info_icon portion" src={require('./images/dinner.svg')}/>
+                            <h3>2人份</h3>
+                        </div>
                     </div>
-                    
-
-
                 </div>
+            </header>
+          </main>
 
-
+          <main className="ingredients_wrap">
+            <h1>所需食材</h1>
+            {/* <span >共2人份</span> */}
+            <div className="ingredients">
+              <div className="ingredient d-flex">
+                <img className="ingredient_pic" src={require('./images/I_chicken.png')}/>
+                <div className="i_text">
+                  <h5 className="i_name">花椰菜</h5>
+                  <p className="i_qty"> 180克</p>
+                </div>
+              </div>
             </div>
-          </div>
+
+          </main>
         </React.Fragment>
       </BrowserRouter>
     );
