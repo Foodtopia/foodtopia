0a917947d78599f3d261fb7a7ef157f928d7ac3f
John Doe <johndoe@example.com>
2018-11-20 20:54:13 +0800

    v4

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -4,6 +4,7 @@ import Recipe from "./recipe/recipe";
 import { Link } from "react-router-dom";
 import Food_recipe from "./food_recipe/food_recipe";
 import Footer from "./footer/footer";
+import $ from 'jquery'
 
 class Content extends Component {
     constructor(props) {
@@ -15,7 +16,9 @@ class Content extends Component {
     this.state = {
         updates:[],
         update:this.initState,
-        type: 'add'
+        type: 'add',
+        inputValue: '使用者の廚房',
+        inputValue2: '歡迎來到我的廚房，一起來做美食吧!'
     }
 }
 
@@ -33,16 +36,41 @@ getUpdate() {
         }))
 };
 
+updateInputValue = (evt) => {
+    var input_data = evt.target.value;
+    this.setState({
+        inputValue:input_data,
+    })
+}
+updateInputValue2 = (evt) => {
+    var input_data2 = evt.target.value
+    this.setState({
+        inputValue2:input_data2,
+    })
+}
+
+change_text1 = () =>{
+    $("h2").toggleClass("active");
+    $(".input1").toggleClass("active");
+}
+change_text2 = () =>{
+    $("p").toggleClass("active");
+    $(".input2").toggleClass("active");
+}
     render() {
         return (
             <React.Fragment>
                 <div className="slider">
                     <div className="blog_text">
                         <div className="header d-flex">
-                            <h2>使用者の廚房</h2><i class="far fa-edit"></i>
+                            <h2>{this.state.inputValue}</h2>
+                            <input className="input input1 active" value={this.state.inputValue} onChange={this.updateInputValue}/>
+                            <i class="far fa-edit" onClick={this.change_text1}></i>
                         </div>
                         <div className="content d-flex">
-                            <p>歡迎來到我的廚房，一起來做美食吧!</p><i class="far fa-edit"></i>
+                            <p>{this.state.inputValue2}</p>
+                            <input className="input input2 active" value={this.state.inputValue2} onChange={this.updateInputValue2}/>
+                            <i class="far fa-edit" onClick={this.change_text2}></i>
                         </div>
                     </div>
                     <div className="blog_text_button">
--- src/components/content/footer/footer.js
+++ src/components/content/footer/footer.js
@@ -5,6 +5,29 @@ import { Link } from "react-router-dom";
 class Footer extends Component {
     constructor(props) {
       super(props);
+      this.initState = {
+        id:'',
+        total_time:'',
+    }
+    this.state = {
+        uploads:[],
+        upload:this.initState,
+        type: 'add'
+    }
+    }
+
+    componentDidMount() {
+        this.getuploads();  
+    }
+
+    getuploads(){
+        fetch("http://localhost:3000/upload/upload_date/")
+        .then(res => res.json())
+        .then(uploads => this.setState({ 
+            uploads: uploads,
+            upload:this.initState,
+            type:'add'
+        }))
     }
 
     render() {
@@ -12,14 +35,40 @@ class Footer extends Component {
         return (
             <React.Fragment>
                 <div className="footer">
-                    <div className="container">
-                        <div className="col-4">
-                        {this.props.updates.map(update => 
-                            <Link className="update_article" to="/">{update.menu}<br /></Link>
+                    <div className="container d-flex footer_width">
+                        <div className="col-4 new_topia">
+                            <div className="blog_title">近期食譜</div>
+                            {this.props.updates.map(update => 
+                                <div className="update_article_border">
+                                    <Link className="update_article" to="/">{update.menu}<br /></Link>
+                                </div>
                             )}
                         </div>
-                        <div className="col-4"></div>
-                        <div className="col-4"></div>
+                        <div className="col-4 new_topia">
+                            <div className="blog_title">分享彙整</div>
+                            {this.state.uploads.map(upload => 
+                                <div className="update_article_border">
+                                    <Link className="update_article" to="/">{upload.total_time}<br /></Link>
+                                </div>
+                            )}
+                        </div>
+                        <div className="col-4 new_topia">
+                            <div className="blog_title">最新迴響</div>
+                            {this.props.updates.map(update => 
+                                <div className="update_article_border">
+                                    <Link className="update_article" to="/"><br /></Link>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className="blog_footer_icon container">
+                        <div className="footer_icon d-flex justify-content-between">
+                            <i class="icon fab fa-facebook col-2"></i>
+                            <i class="icon fab fa-instagram col-2"></i>
+                            <i class="icon fab fa-google-plus-g col-2"></i>
+                            <i class="icon fab fa-youtube col-2"></i>
+                            <i class="icon far fa-envelope col-2"></i>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
